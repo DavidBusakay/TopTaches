aba056e5e6aaf93a34dfce2bdfbc96ada8696f1d
DavidBusakay <davidbusakay14@gmail.com>
2026-01-13 01:24:42 +0100

    Modification du layout

--- app/_layout.js
+++ app/_layout.js
@@ -1,14 +1,33 @@
 import Colors from "@/constants/Colors";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
 import { StatusBar } from "react-native";
 import FlashMessage from "react-native-flash-message";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AppNavigator from "./navigations/AppNavigator";
 
 const RootLayout = () => {
+  const [initialRoute, setInitialRoute] = useState(null);
+  const [appReady, setAppReady] = useState(false);
+
+  useEffect(() => {
+    const checkUser = async () => {
+      try {
+        const user = await AsyncStorage.getItem("user");
+        setInitialRoute(user ? "Main" : "Onboarding");
+      } finally {
+        setAppReady(true);
+      }
+    }
+    checkUser();
+  }, []);
+
+  if (!appReady || !initialRoute) return null;
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <StatusBar barStyle={"light-content"} backgroundColor={Colors.primary} />
-      <AppNavigator />
+      <AppNavigator initialRouteName={initialRoute} />
       <FlashMessage position="bottom" />
     </GestureHandlerRootView>
   );
--- app/navigations/AppNavigator.js
+++ app/navigations/AppNavigator.js
@@ -1,14 +1,15 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import OnboardingSreen from "../screens/OnboarbingScreen";
-import SplashScreen from "../screens/SplashScreen";
 import DrawerNavigator from "./DrawerNavigator";
 
 const Stack = createNativeStackNavigator();
 
-const AppNavigator = () => {
+const AppNavigator = ({ initialRouteName }) => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false, animation: "fade" }}>
-      <Stack.Screen name="SplashScreen" component={SplashScreen} />
+    <Stack.Navigator
+      screenOptions={{ headerShown: false, animation: "fade" }}
+      initialRouteName={initialRouteName}
+    >
       <Stack.Screen name="Onboarding" component={OnboardingSreen} />
       <Stack.Screen name="Main" component={DrawerNavigator} />
     </Stack.Navigator>
